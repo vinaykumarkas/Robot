1f57b88580be5b899f2a342341af4481c8a14b68
Vinaya kumar reddy kasirala <KVK.Reddy@outlook.com>
2014-07-02 19:31:56 +1000

    Added main class - used to get commands from command line and execute

---
+++ app/robot_replica.rb
@@ -0,0 +1,11 @@
+require_relative 'robot_simulator'
+
+simulator = Simulator.new
+puts "Please command the Robot: "
+command = STDIN.gets
+
+while command
+  output = simulator.execute(command.to_s.upcase)
+  puts output if output
+  command = STDIN.gets
+end
